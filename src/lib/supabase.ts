f25405c6234f61ee3b1ea355405ecda174052cc6
Dhamivibez <hello@dhamivibez.name.ng>
2025-10-29 21:33:20 +0100

    feat(deps): add supabase client setup

---
+++ src/lib/supabase.ts
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl: string = import.meta.env.VITE_SUPABASE_URL
+const anonKey: string = import.meta.env.VITE_SUPABASE_ANON_KEY
+
+export const supabase = createClient(supabaseUrl, anonKey)
